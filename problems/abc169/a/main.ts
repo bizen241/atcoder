df041d530779e7b62f6106eea29d5eb35122c7a8
bizen241 <bizen241@gmail.com>
2020-06-06 13:42:31 +0900

    solve abc169_a

---
+++ problems/abc169/a/main.ts
@@ -0,0 +1,9 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("/dev/stdin", "utf8").split("\n");
+
+const [a, b] = input[0].split(" ").map(Number);
+
+const answer = a * b;
+
+console.log(answer);

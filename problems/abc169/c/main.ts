510e285e3bb47fca9f45ba6dfc9131e79353b581
bizen241 <bizen241@gmail.com>
2020-06-07 19:31:57 +0900

    solve abc169_c

---
+++ problems/abc169/c/main.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from "fs";
+
+const main = (input: string[][]) => {
+  const A = BigInt(input[0][0]);
+  const B = BigInt(Math.round(Number(input[0][1]) * 100));
+
+  const prod = (A * B) / 100n;
+
+  return prod.toString();
+};
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+console.log(main(read()));

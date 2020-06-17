a35324c5bd7e609272da97fd735fbb6b6daba05b
bizen241 <bizen241@gmail.com>
2020-06-17 23:00:10 +0900

    solve abc167_a

---
+++ problems/abc167/a/main.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const S = input[0][0];
+  const T = input[1][0];
+
+  return T.slice(0, -1) === S ? "Yes" : "No";
+};
+
+console.log(main(read()));

e216d2266ce0be65ab264904e2e5e32a8be3bfa2
bizen241 <bizen241@gmail.com>
2020-06-20 01:18:49 +0900

    solve abc003_2

---
+++ problems/abc003/b/main.ts
@@ -0,0 +1,28 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const S = input[0][0];
+  const T = input[1][0];
+
+  for (let i = 0; i < S.length; i++) {
+    const s = S[i];
+    const t = T[i];
+
+    if (
+      (s === "@" && !"@atcoder".includes(t)) ||
+      (t === "@" && !"@atcoder".includes(s)) ||
+      (s !== "@" && t !== "@" && s !== t)
+    ) {
+      return "You will lose";
+    }
+  }
+
+  return "You can win";
+};
+
+console.log(main(read()));

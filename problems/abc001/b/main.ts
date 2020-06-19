647d6a82f4e7df14febdb7ac109bb8eb5051e6ae
bizen241 <bizen241@gmail.com>
2020-06-19 17:31:54 +0900

    update abc001_2

--- problems/abc001/b/main.ts
+++ problems/abc001/b/main.ts
@@ -5,23 +5,20 @@ const read = () =>
     .split("\n")
     .map((line) => line.split(" "));
 
-const main = (input: string[][]) => {
+const main = (input: string[][]): string => {
   const m = Number(input[0][0]);
 
   if (m < 100) {
     return "00";
-  }
-  if (m <= 5000) {
+  } else if (m <= 5000) {
     return (m / 100).toString().padStart(2, "0");
-  }
-  if (m <= 30000) {
+  } else if (m <= 30000) {
     return (m / 1000 + 50).toString();
-  }
-  if (m <= 70000) {
+  } else if (m <= 70000) {
     return ((m / 1000 - 30) / 5 + 80).toString();
+  } else {
+    return "89";
   }
-
-  return "89";
 };
 
 console.log(main(read()));

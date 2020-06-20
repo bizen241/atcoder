00d0c9ee30eae5c932e5c19b446f50536ffb19cf
bizen241 <bizen241@gmail.com>
2020-06-20 09:47:39 +0900

    solve abc006_1

---
+++ problems/abc006/a/main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const N = Number(input[0][0]);
+
+  return N % 3 === 0 ? "YES" : "NO";
+};
+
+console.log(main(read()));

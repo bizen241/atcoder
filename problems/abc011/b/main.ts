36414b6ebe24ecc8867eaa775274e910d801a96c
bizen241 <bizen241@gmail.com>
2020-06-20 19:12:26 +0900

    solve abc011_2

---
+++ problems/abc011/b/main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const S = input[0][0];
+
+  return `${S[0].toUpperCase()}${S.slice(1).toLowerCase()}`;
+};
+
+console.log(main(read()));

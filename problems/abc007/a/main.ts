a70509a4b83cef67c3caf8b65bcbf0666247d474
bizen241 <bizen241@gmail.com>
2020-06-20 10:18:56 +0900

    solve abc007_1

---
+++ problems/abc007/a/main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const n = Number(input[0][0]);
+
+  return n - 1;
+};
+
+console.log(main(read()));

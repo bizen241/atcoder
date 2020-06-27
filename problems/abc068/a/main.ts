946aa6d0e35347a36f4045740e5507902d2cdab4
bizen241 <bizen241@gmail.com>
2020-06-27 12:19:53 +0900

    solve abc068_a

---
+++ problems/abc068/a/main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const N = input[0][0];
+
+  return `ABC${N}`;
+};
+
+console.log(main(read()));

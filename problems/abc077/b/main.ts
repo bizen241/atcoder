ce0ca98f9a80fbe540300856dd269549e264e796
bizen241 <bizen241@gmail.com>
2020-06-27 14:54:45 +0900

    solve abc077_b

---
+++ problems/abc077/b/main.ts
@@ -0,0 +1,26 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const N = Number(input[0][0]);
+
+  let answer = 0;
+
+  for (let i = 1; i; i++) {
+    const squareNumber = i * i;
+
+    if (squareNumber > N) {
+      break;
+    }
+
+    answer = squareNumber;
+  }
+
+  return answer;
+};
+
+console.log(main(read()));

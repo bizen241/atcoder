c10b4cdbe8d22c6d88ef9574f47acbec1a0f6307
bizen241 <bizen241@gmail.com>
2020-06-28 14:50:11 +0900

    solve abc093_a

---
+++ problems/abc093/a/main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const S = input[0][0];
+
+  return new Set(S).size === 3 ? "Yes" : "No";
+};
+
+console.log(main(read()));

74419f5893c9edb704059162a1f8605add431669
bizen241 <bizen241@gmail.com>
2020-07-15 14:45:18 +0900

    solve abc109_a

---
+++ problems/abc109/a/main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const [A, B] = input[0].map(Number);
+
+  return A !== 2 && B !== 2 ? "Yes" : "No";
+};
+
+console.log(main(read()));

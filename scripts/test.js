aa5c5743d33f95a78379378ec8c93809d0b5099c
bizen241 <bizen241@gmail.com>
2021-01-01 16:46:17 +0900

    update test script

---
+++ scripts/test.js
@@ -0,0 +1,33 @@
+const { spawn } = require("child_process");
+
+const commands = {
+  ts: {
+    compile: "tsc ./main.ts --target ESNext --moduleResolution Node --module CommonJS",
+    execute: "node main.js",
+  },
+  kt: {
+    compile: "kotlinc ./main.kt -include-runtime -d ./main.jar -XXLanguage:+InlineClasses",
+    execute: "/usr/lib/jvm/java-11-openjdk-amd64/bin/java -cp ./main.jar MainKt",
+  },
+};
+
+const options = {
+  cwd: process.env.INIT_CWD,
+  stdio: "inherit",
+};
+const oj = (c) => spawn("oj", ["t", "-d", "./tests", "-c", c], options)
+
+const lang = commands[process.argv[2]];
+if (lang === undefined) {
+  process.exit();
+}
+
+const { compile, execute } = lang;
+
+if (!compile) {
+  oj(execute);
+} else {
+  const [command, ...args] = compile.split(" ");
+
+  spawn(command, args, options).on("close", () => oj(execute));
+}

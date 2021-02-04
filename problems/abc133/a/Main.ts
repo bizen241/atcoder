105f4def0f7559f26f924f129b70dc9bf2f16695
bizen241 <bizen241@gmail.com>
2021-02-05 06:05:08 +0900

    ts: abc133_a

---
+++ problems/abc133/a/Main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const [N, A, B] = input[0].map(Number);
+
+  return Math.min(A * N, B);
+};
+
+console.log(main(read()));

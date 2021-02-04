f3ef975967aaa93f5676aa14e3d31eb002ff833f
bizen241 <bizen241@gmail.com>
2021-02-05 05:49:50 +0900

    ts: abc131_a

---
+++ problems/abc131/a/Main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const S = input[0][0];
+
+  return /(.)\1/.test(S) ? "Bad" : "Good";
+};
+
+console.log(main(read()));

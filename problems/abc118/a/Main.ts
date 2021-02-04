d1eb9c1dfd783092553aaee1e2c89b943846c6d4
bizen241 <bizen241@gmail.com>
2021-02-04 21:29:47 +0900

    ts: abc118_a

---
+++ problems/abc118/a/Main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const [A, B] = input[0].map(Number);
+
+  return B % A === 0 ? A + B : B - A;
+};
+
+console.log(main(read()));

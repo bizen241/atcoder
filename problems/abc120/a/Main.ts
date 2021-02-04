190892b2ced973700492aff975d76a89f41ca3b7
bizen241 <bizen241@gmail.com>
2021-02-04 21:35:24 +0900

    ts: abc120_a

---
+++ problems/abc120/a/Main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const [A, B, C] = input[0].map(Number);
+
+  return Math.min(Math.floor(B / A), C);
+};
+
+console.log(main(read()));

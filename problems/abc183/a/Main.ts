8d2330352789f8520be54661631ce1c8608e88c2
bizen241 <bizen241@gmail.com>
2021-02-06 08:40:11 +0900

    ts: abc183_a

---
+++ problems/abc183/a/Main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const x = Number(input[0][0]);
+
+  return x >= 0 ? x : 0;
+};
+
+console.log(main(read()));

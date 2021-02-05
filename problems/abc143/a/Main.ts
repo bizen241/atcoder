345e3c6e4b84a8ed69276192095aebfdebce8b5c
bizen241 <bizen241@gmail.com>
2021-02-05 18:25:38 +0900

    ts: abc143_a

---
+++ problems/abc143/a/Main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const [A, B] = input[0].map(Number);
+
+  return Math.max(0, A - B * 2);
+};
+
+console.log(main(read()));

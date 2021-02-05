f87322f774c0167d1a29190da9360d370185349d
bizen241 <bizen241@gmail.com>
2021-02-05 18:27:26 +0900

    ts: abc144_a

---
+++ problems/abc144/a/Main.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+const read = () =>
+  readFileSync(0, "utf8")
+    .split("\n")
+    .map((line) => line.split(" "));
+
+const main = (input: string[][]) => {
+  const [A, B] = input[0].map(Number);
+
+  return A <= 9 && B <= 9 ? A * B : -1;
+};
+
+console.log(main(read()));
